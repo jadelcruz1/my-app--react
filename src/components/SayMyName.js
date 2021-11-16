940a79b007900e9267f46a5daf26fc1604754a00
jadelcruz1 <jardelcruz6@gmail.com>
2021-11-16 14:45:37 -0300

    inserir css no React js de maneira especfica

--- src/components/SayMyName.js
+++ src/components/SayMyName.js
@@ -1,11 +1,13 @@
+import styles from './SayMyName.module.css'
+
 function SayMyName({ nome, idade, cor, altura}){
     return(
         <div>
             
             estudando props simples
             <p> nome:{nome} </p>
-            <p> Idade:{idade} </p>
-            <p> Cor:{cor} </p>
+            <p className={ styles.teste}> Idade:{idade} </p>
+            <p className={ styles.teste2}> Cor:{cor} </p>
             <p> Altura:{altura} </p>
 
     </div>
